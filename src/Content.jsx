32d41d307c3cfeb9b34be04f9423cd0bae9acb3e
divyKS <divycodescompetes@gmail.com>
2023-07-08 10:13:36 +0530

    Chapter5 Done: onClick, onDoubleClick, event object

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,9 +6,33 @@ const Content = () => {
     return names[index]
   }
 
+  function handleClick(){
+    console.log("The button was clicked!")
+  }
+
+  function handleClick2(name){
+    console.log(`${name} was clicked!`)
+  }
+
+  function handleClick3(e){
+    console.log(e)
+    console.log(e.target)
+    console.log(e.target.innerHTML)
+  }
+
   return (
     <main>
-        Hello {handleGetName()}!
+        <article>
+          <p onDoubleClick={handleClick}>
+            {/* to click some button and change the name here, we will have to use hook: useState */}
+            Hello {handleGetName()}!
+          </p>
+          {/* here too, we didn't use (), we just passed in the reference to that function */}
+          <button onClick={handleClick}>Click Me</button>&nbsp;
+          {/* we have to make an anonymous function in jsx to call the function with an argument to avoid immediate execution on rendering */}
+          <button onClick={()=>handleClick2("Dave")}>Click Me</button>&nbsp;
+          <button onClick={(e)=>handleClick3(e)}>Click Me</button>
+        </article>
     </main>
   )
 }
