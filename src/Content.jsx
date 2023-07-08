94dbae59900863cfe9c26343bbab50bd5a686dc7
divyKS <divycodescompetes@gmail.com>
2023-07-08 11:23:07 +0530

    Chapter6 Done: useState

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,38 +1,33 @@
-const Content = () => {
-  
-  function handleGetName(){
-    const names = ["Divy", "Chunmun", "Nikku"]
+import { useState } from "react"
+
+function Content(){
+  // array destructuring that we get from useState
+  const [name, setName] = useState("DEFAULT")
+  const [count, setCount] = useState(0)
+
+  function handleNameChange(){
+    console.log("Button clicked")
+    const nameChoices = ["Divy", "Chunmun", "Nikku"]
     const index = Math.floor(Math.random()*3)
-    return names[index]
+    setName(nameChoices[index])
   }
 
-  function handleClick(){
-    console.log("The button was clicked!")
-  }
-
-  function handleClick2(name){
-    console.log(`${name} was clicked!`)
-  }
-
-  function handleClick3(e){
-    console.log(e)
-    console.log(e.target)
-    console.log(e.target.innerHTML)
+  function handleCount(){
+    setCount(count + 1)
+    // for the console.log() the value of count remains the same as the one at the time of fn call 
+    console.log(count)
   }
 
   return (
     <main>
-        <article>
-          <p onDoubleClick={handleClick}>
-            {/* to click some button and change the name here, we will have to use hook: useState */}
-            Hello {handleGetName()}!
-          </p>
-          {/* here too, we didn't use (), we just passed in the reference to that function */}
-          <button onClick={handleClick}>Click Me</button>&nbsp;
-          {/* we have to make an anonymous function in jsx to call the function with an argument to avoid immediate execution on rendering */}
-          <button onClick={()=>handleClick2("Dave")}>Click Me</button>&nbsp;
-          <button onClick={(e)=>handleClick3(e)}>Click Me</button>
-        </article>
+        <p>
+          Hello {name}! &nbsp;
+          <button onClick={handleNameChange}>Change Name</button>
+        </p>
+        <p>
+          Count: {count} &nbsp;
+          <button onClick={handleCount}>Increase Count</button>
+        </p>
     </main>
   )
 }
